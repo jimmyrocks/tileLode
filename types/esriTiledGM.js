cfe227cbb433cea21d786342bb235a8d94f6e152
Jim McAndrew <jim@loc8.us>
2013-09-15 10:54:08 -0600

    Still trying to get gm to work, might just use image magick

--- types/esriTiledGM.js
+++ types/esriTiledGM.js
@@ -1,5 +1,5 @@
-var streamify = require('streamify'),
-    gm = require('gm'),
+var stream = require('stream'),
+    spawn = require('child_process').spawn,
     fs = require('fs');
 
 exports.process = function process (req, res, config) {
@@ -21,20 +21,28 @@ exports.process = function process (req, res, config) {
         tileUrl = tileUrl.replace("%y",y);
     }
 
-    // Graphicsmagick stuff
-//    var g = gm();
-    var imgReadStream = new streamify();
-                res.writeHead(200, {'Content-Type': 'image/jpeg'});
-    imgReadStream.addDest(res);
-/*
-    gm(imgReadStream)
-    .resize(128, 128)
-    .stream(function(err, stdout, stderr){
-//        stdout.pipe(res);
-        //console.log(stderr);
+    // Do some image magick!
+    //var args = ['-', 'gif:-'];
+    var args = ['-', '-thumbnail', '512x512^', '-'];
+    var convert = spawn('convert ' + args.join(' '), []);
+    var imgReadStream = convert.stdin;
+    res.writeHead(200, {'Content-Type': 'image/gif'});
+    convert.stdout.on('data', function(data) {
+        console.log("y");
+        console.log(data);
+        res.write(data);
     });
-    };
-    */
+    convert.stdout.on('end', function(data) {
+        console.log("z");
+        console.log(data);
+        res.end(data);
+    });
+
+    convert.on('close', function(code) {console.log("closed with code: " + code);});
+    convert.stderr.on('data', function(data) {console.log("convert.stderr: " + data);});
+    convert.on('error', function(err){console.log("convert err: " , err);});
+    convert.stdin.on('error', function(data) {console.log("convert.stdin error: ", data);});
+
     return {
         "url" :tileUrl,
         "cachePath" : [urlParams[1], urlParams[2], urlParams[3], urlParams[4]].join("/"),
