a1e030cff4b038c82cdce2b0143b3a930670560c
Jim McAndrew <jim@loc8.us>
2013-09-15 19:33:53 -0600

    fixed up the Imagemagick code, but it still needs a lot of work

--- types/esriTiled.js
+++ types/esriTiled.js
@@ -2,26 +2,49 @@ exports.process = function process (req, res, config) {
     var z,
         x,
         y,
-        tileUrl,
-        urlParams;
+        urlParams = req.url.replace(/[^0-9a-zA-Z]/g, "/").split('/'),
+        tileObject = {
+            "tileUrl": null,
+            "cachePath": null,
+            "cacheTile": false,
+            "returnTile": false,
+            "res": res,
+            "stream": res,
+            "imageFormat": config.imageFormat
+        };
 
-    urlParams = req.url.replace(/[^0-9a-zA-Z]/g, "/").split('/');
     if (urlParams.length > 3) {
         // We have enough params
         z = urlParams[2];
         x = urlParams[3];
         y = urlParams[4];
 
-        tileUrl = config.url.replace("%z",z);
-        tileUrl = tileUrl.replace("%x",x);
-        tileUrl = tileUrl.replace("%y",y);
+        tileObject.tileUrl = config.url.replace("%z",z);
+        tileObject.tileUrl = tileObject.tileUrl.replace("%x",x);
+        tileObject.tileUrl = tileObject.tileUrl.replace("%y",y);
+        tileObject.cachePath = urlParams.splice(1,5).join("/");
+
+        // If we're not in the right zoom, return false for the tileUrl
+        console.log("zoom, min, max: ", z, config.minZoom, config.maxZoom);
+        if (z >= parseInt(config.minZoom,10) && z <= parseInt(config.maxZoom, 10)) {
+        console.log("$zoom, min, max: ", z, config.minZoom, config.maxZoom);
+            tileObject.cacheTile = config.cacheTiles;
+            tileObject.returnTile = true;
+        } else {
+            tileObject.errorDescription = [
+                "The zoom level of (",
+                z,
+                ") is not between the minZoom (",
+                config.minZoom,
+                ") and the maxZoom (",
+                config.maxZoom,
+                ")."
+            ].join('');
+        }
+    } else {
+        tileObject.errorDescription = "Too few parameters, this requires parameters in z/y/x format";
     }
-    return {
-        "url" :tileUrl,
-        "cachePath" : [urlParams[1], urlParams[2], urlParams[3], urlParams[4]].join("/"),
-        "res": res,
-        "stream": res
-    };
+    return tileObject;
 };
 
 
