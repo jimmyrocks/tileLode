7e428aca3a2dda80e3b08355ee398322cfda128d
Jim McAndrew <jim@loc8.us>
2015-01-04 17:16:20 -0700

    Clean up code: Update src/tools/imageTypes.js

--- src/tools/imageTypes.js
+++ src/tools/imageTypes.js
@@ -2,16 +2,16 @@
 // http://en.wikipedia.org/wiki/Internet_media_type#Type_image
 
 var types = {
-    "gif": "image/gif",
-    "jpg": "image/jpeg",
-    "jpeg": "image/jpeg",
-    "png": "image/png",
-    "svg": "image/svg+xml",
-    "tiff": "image/tiff"
+  'gif': 'image/gif',
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'png': 'image/png',
+  'svg': 'image/svg+xml',
+  'tiff': 'image/tiff'
 };
 
-for (type in types) {
-    if (types.hasOwnProperty(type)) {
-        exports[type] = types[type];
-    }
+for (var type in types) {
+  if (types.hasOwnProperty(type)) {
+    exports[type] = types[type];
+  }
 }
